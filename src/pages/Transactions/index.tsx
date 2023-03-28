bf01e27e65ce9d82f4cc84813858fcb04d399164
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-28 17:08:35 -0300

    feat: table with values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,61 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { PriceHighLight, TransactionContainer, TransactionTable } from './styles';
 
 export function Transactions(){
   return (
     <>
     <Header/>
     <Summary />
+    <TransactionContainer>
+    <TransactionTable>
+      <tbody>
+        <tr>
+          <td width="50%">Desenvolvimento de site</td>
+          <td>
+            <PriceHighLight variant="income">
+              R$ 6.750,00
+            </PriceHighLight>
+            </td>
+          <td>
+            Venda
+            </td>
+          <td>27/03/2023</td>
+        </tr>
+        <tr>
+          <td width="50%">Infra AWS</td>
+          <td>
+          <PriceHighLight variant="outcome">
+              - R$ 3.750,00
+            </PriceHighLight>
+            </td>
+          <td>Gastos</td>
+          <td>22/03/2023</td>
+        </tr>
+        <tr>
+          <td width="50%">Desenvolvimento de site</td>
+          <td>
+          <PriceHighLight variant="income">
+              R$ 6.750,00
+            </PriceHighLight>
+            </td>
+          <td>Venda</td>
+          <td>27/03/2023</td>
+        </tr>
+        <tr>
+          <td width="50%">Desenvolvimento de site</td>
+          <td>    
+            <PriceHighLight variant="income">
+              R$ 6.750,00
+            </PriceHighLight>
+            </td>
+          <td>Venda</td>
+          <td>27/03/2023</td>
+        </tr>
+      </tbody>
+    </TransactionTable>
+    </TransactionContainer>
+    
     </>
   )
 }
