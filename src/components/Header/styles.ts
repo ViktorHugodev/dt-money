6de4c6ec634a106ff8214a788927590dcfe87188
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-28 16:02:45 -0300

    initial config styled

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  background-color: ${props => props.theme['gray-900']};
+  padding: 2.5rem 0 7.5rem;
+
+`
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+`
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+  border: 0;
+  background-color: ${props => props.theme['green-500']};
+  padding: 0 1.25rem;
+  font-weight: bold;
+  border-radius: 6px;
+  color: ${props => props.theme['white']};
+  &:hover {
+    background-color: ${props => props.theme['green-700']};
+  }
+
+`
