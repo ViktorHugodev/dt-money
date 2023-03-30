4e47856e0ea9fe3180adf7580a045fd240039df0
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-30 08:23:55 -0300

    feat: add json server, transactions page request

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,63 +1,59 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { SearchForm } from '../../components/SearchForm';
 import { Summary } from '../../components/Summary';
 import { PriceHighLight, TransactionContainer, TransactionTable } from './styles';
 
-export function Transactions(){
+interface TransactionProps {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  category: string
+  price: number
+  createdAt: string
+}
+
+export function Transactions() {
+  const [transactions, setTransactions] = useState<TransactionProps[]>([])
+  async function getTransactionsData() {
+    const response = await fetch('http://localhost:3331/transactions')
+    const data = await response.json()
+    setTransactions(data)
+  }
+  useEffect(() => {
+    getTransactionsData()
+  }, [])
+
   return (
     <>
-    <Header/>
-    <Summary />
-    <TransactionContainer>
-      <SearchForm />
-    <TransactionTable>
-      <tbody>
-        <tr>
-          <td width="50%">Desenvolvimento de site</td>
-          <td>
-            <PriceHighLight variant="income">
-              R$ 6.750,00
-            </PriceHighLight>
-            </td>
-          <td>
-            Venda
-            </td>
-          <td>27/03/2023</td>
-        </tr>
-        <tr>
-          <td width="50%">Infra AWS</td>
-          <td>
-          <PriceHighLight variant="outcome">
-              - R$ 3.750,00
-            </PriceHighLight>
-            </td>
-          <td>Gastos</td>
-          <td>22/03/2023</td>
-        </tr>
-        <tr>
-          <td width="50%">Desenvolvimento de site</td>
-          <td>
-          <PriceHighLight variant="income">
-              R$ 6.750,00
-            </PriceHighLight>
-            </td>
-          <td>Venda</td>
-          <td>27/03/2023</td>
-        </tr>
-        <tr>
-          <td width="50%">Desenvolvimento de site</td>
-          <td>    
-            <PriceHighLight variant="income">
-              R$ 6.750,00
-            </PriceHighLight>
-            </td>
-          <td>Venda</td>
-          <td>27/03/2023</td>
-        </tr>
-      </tbody>
-    </TransactionTable>
-    </TransactionContainer>
-    
+      <Header />
+      <Summary />
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>
+                    {transaction.category}
+                  </td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
+
+
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+
     </>
   )
 }
